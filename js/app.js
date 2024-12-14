d001bfd1a43a73b2a21db07298204b5655aa7cef
p4f-4-fun <plenczewski37@gmail.com>
2024-12-14 19:41:59 +0100

    objective it means class approach

--- js/app.js
+++ js/app.js
@@ -1,24 +1,75 @@
 'use strict';
 
-let playerLSData = {
-    playerName: localStorage.getItem("playerName") || "Player 1",
-    playerScore: localStorage.getItem("playerScore") || 0,
+// GLOBALS
+const canvas = document.getElementById("game--container__canvas");
+const ctx = canvas.getContext("2d");
+// /GLOBALS
+
+// CLASSES
+class CGameView {
+    constructor() {
+        this.renderGameScore();
+    }
+
+    renderGameScore() {
+        const scoreElement = document.querySelector(".leaderboard--span__score");
+        const score = OCPlayer.playerScore;
+        scoreElement.innerHTML = score;
+    }
+}
+
+class CCursor {
+    constructor() {
+        this.cursorPositionX = 0;
+        this.cursorPositionY = 0;
+
+    }
+
+    updateCursorPosition (e) {
+        this.cursorPositionX = e.offsetX;
+        this.cursorPositionY = e.offsetY;
+    }
+}
+
+class CPlayer {
+    #playerLSData = {};  // Local Storage data for player;
+    constructor() {
+        this.#playerLSData = {
+            playerName: localStorage.getItem("playerName") || "Player 1",
+            playerScore: localStorage.getItem("playerScore") || 0, 
+        };
+    }
+
+    get playerName() {
+        return this.#playerLSData.playerName;
+    }
+    set playerName(playerName) {
+        this.#playerLSData.playerName = playerName;
+        localStorage.setItem("playerName", this.#playerLSData.playerName);
+    }
+    
+    get playerScore() {
+        return this.#playerLSData.playerScore;
+    }
+    set playerScore(playerScore) {
+        this.#playerLSData.playerScore = playerScore;
+        localStorage.setItem("playerScore", this.#playerLSData.playerScore);
+    }
+}
+const OCPlayer = new CPlayer();
+const OCCursor = new CCursor();
+const OCGameView = new CGameView();
+// /CLASSES
+
+// EVENT BINDINGS
+canvas.addEventListener("mousemove", OCCursor.updateCursorPosition);
+// /EVENT BINDINGS
+
+// APP FUNCTION
+const initGameLoop = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //requestAnimationFrame(initGameLoop);
 };
 
-const setPlayerName = (playerName) => {
-    playerLSData.playerName = playerName;
-    localStorage.setItem("playerName", playerLSData.playerName);
-};
-
-const updatePlayerScore = (playerScore) => {
-    playerLSData.playerScore = playerScore;
-    localStorage.setItem("playerScore", playerLSData.playerScore);
-};
-
-const init = () => {
-    console.log('app() init');
-    console.log(`Hi, ${playerLSData.playerName}!`);
-};
-
-
-init();
+initGameLoop();
+// /APP FUNCTION
